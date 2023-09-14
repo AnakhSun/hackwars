29ddf1307b03f9bc6c6797b2ddc8143a6bc7b279
Ivan Churilov <ivanvrn2003@gmail.com>
2023-09-14 10:00:22 +0300

    made func for creation palindromic numbers

---
+++ palindromic numbers.py
@@ -0,0 +1,35 @@
+
+def palindromize(number):
+    if number != 196:
+        number = str(number)
+        flag = 0
+        iterations  = 0
+        while flag == 0:
+            flag = 1
+            if len(number) % 2 == 1:
+                n = (len(number) - 1) // 2
+                for i in range(0, n):
+                    if number[i] != number[n*2-i]:
+                        flag = 0
+                        iterations += 1
+                        # palindromize(number, iterations)
+                        break
+
+            if len(number) % 2 == 0:
+                n = (len(number)) // 2
+                for i in range(0, n):
+                    if number[i] != number[n*2-i-1]:
+                        flag = 0
+                        iterations += 1
+                        # palindromize(number, iterations)
+                        break
+            
+            if flag == 1:
+                res = int(number)
+            number = str(int(str(number)) + int(str(number)[::-1]))
+
+        return str(iterations) + " " + str(res)
+    else:
+        return None
+       
+print(palindromize(195))
